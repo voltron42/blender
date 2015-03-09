9fd2564c233e1f0ab22ff63f0fba7b7c76e2a560
Daniel Johnson <dajohnson1978@gmail.com>
2015-03-08 22:38:11 -0500

    core algorithm for expectable.js

--- testing/Expectable/expectable.js
+++ testing/Expectable/expectable.js
@@ -1,184 +1,4 @@
 (function() {
-	var raw = {
-		"equal": {
-			"msg": "Values are not equal:\n\tActual: $$$,\n\tExpected: $$$",
-			"not": "Values are equal:\n\tActual: $$$,\n\tExpected: $$$",
-			"params": "expected",
-			"cond": "this == expected"
-		},
-		"deepEqual": {
-			"msg": "Values are not equal:\n\tActual: $$$,\n\tExpected: $$$",
-			"not": "Values are equal:\n\tActual: $$$,\n\tExpected: $$$",
-			"params": "expected",
-			"cond": "deep.eq(this,expected)"
-		},
-		"exist": {
-			"msg": "Value does not exist!",
-			"not": "Value exists:\n\tValue:$$$",
-			"params": "",
-			"cond": "typeof this != undefined && this != null"
-		},
-		"match": {
-			"msg": "",
-			"not": "",
-			"params": "regex",
-			"cond": "new RegExp(regex).test(this)"
-		},
-		"be": {
-			"*": {
-				"msg": "",
-				"not": "",
-				"params": "expected",
-				"cond": "this === expected"
-			},
-			"a": {
-				"msg": "",
-				"not": "",
-				"params": "type",
-				"cond": "typeof this == type"
-			},
-			"anInstanceOf": {
-				"msg": "",
-				"not": "",
-				"params": "constructor",
-				"cond": "this instanceof constructor"
-			},
-			"undefined:": {
-				"msg": "",
-				"not": "",
-				"params": "",
-				"cond": "typeof this == 'undefined'"
-			},
-			"null": {
-				"msg": "",
-				"not": "",
-				"params": "",
-				"cond": "this == null"
-			},
-			"ok": {
-				"msg": "",
-				"not": "",
-				"params": "",
-				"cond": "this?true:false"
-			},
-			"empty": {
-				"msg": "",
-				"not": "",
-				"params": "",
-				"cond": "((this instanceof Array) || typeof this == 'string')&& this.length == 0) || (typeof this == 'object' && Object.keys(this).length == 0)"
-			},
-			"true": {
-				"msg": "",
-				"not": "",
-				"params": "",
-				"cond": "this === true"
-			},
-			"false": {
-				"msg": "",
-				"not": "",
-				"params": "",
-				"cond": "this === false"
-			},
-			"lessThan": {
-				"msg": "",
-				"not": "",
-				"params": "bound",
-				"cond": "this < bound"
-			},
-			"greaterThan": {
-				"msg": "",
-				"not": "",
-				"params": "bound",
-				"cond": "this > bound"
-			},
-			"within": {
-				"msg": "",
-				"not": "",
-				"params": "low, high",
-				"cond": "this <= high && value >= low"
-			}
-		},
-		"have": {
-			"any": {
-				"*": {
-					"msg": "",
-					"not": "",
-					"params": "",
-					"cond": "contains.any(this, arguments)"
-				},
-				"keys": {
-					"msg": "",
-					"not": "",
-					"params": "",
-					"cond": "contains.any(Object.keys(this), arguments)"
-				}
-			},
-			"all": {
-				"*": {
-					"msg": "",
-					"not": "",
-					"params": "",
-					"cond": "contains.all(this, arguments)"
-				},
-				"keys": {
-					"msg": "",
-					"not": "",
-					"params": "",
-					"cond": "contains.all(Object.keys(this), arguments)"
-				}
-			},
-			"length": {
-				"*": {
-					"msg": "",
-					"not": "",
-					"params": "bound",
-					"cond": "this.length == bound"
-				},
-				"lessThan": {
-					"msg": "",
-					"not": "",
-					"params": "bound",
-					"cond": "this.length < bound"
-				},
-				"greaterThan": {
-					"msg": "",
-					"not": "",
-					"params": "bound",
-					"cond": "this.length > bound"
-				},
-				"within": {
-					"msg": "",
-					"not": "",
-					"params": "low, high",
-					"cond": "this.length <= high && this.length >= low"
-				}
-			},
-			"property": {
-				"msg": "",
-				"not": "",
-				"params": "key, value",
-				"cond": "this[key] == value"
-			},
-			"ownProperty": {
-				"msg": "",
-				"not": "",
-				"params": "key",
-				"cond": "key in this"
-			},
-			"deepProperty": {
-				"msg": "",
-				"not": "",
-				"params": "key, value",
-				"cond": "deep.prop(this,key,value)"
-			}
-		},
-		"contain": {
-			"msg": "Substring is not present in value:\n\tValue: $$$,\n\tSubstring: $$$",
-			"not": "Substring is present in value:\n\tValue: $$$,\n\tSubstring: $$$",
-			"params": "needle",
-			"cond": "this.indexOf(needle) >= 0"
-		}
-	};
 	var deep = {
 		eq:function(a, b) {
 			
@@ -195,24 +15,43 @@
 			
 		}
 	}
-	var Body = function(message, compare) {
-		this.check = function(value) {
-			return function() {
-				if (!compare.apply(value,arguments)) {
-					throw new Error(message)
-				}
-			}
-		}
-		this.negate = function(msg) {
-			return new Body(msg,function() {
-				return (!compare.apply(null,arguments));
-			});
-			}
-		}
-	}
+	var raw = expecting.map(function(spec){
+  	return {
+    	path:spec.path,
+    	msg:spec.msg,
+    	not:spec.not,
+    	fn:eval("function("+spec.params+"){return "+spec.cond+"};")
+  	};
+	});
+	var build = function(root,label,actual) {
+  	return function(spec) {
+    	var path = spec.path.split(".");
+    	var name = path.pop();
+    	var temp = root;
+    	path.forEach(function(step){
+      	temp[step] = temp[step] || {};
+      	temp = temp[step];
+    	});
+    	var fn = function() {
+      	if (!spec.fn.apply(actual,arguments)) {
+        	throw new Error(spec[label]);
+      	}
+    	};
+    	if (typeof temp[name] == 'object') {
+      	Object.keys(temp[name]).forEach(function(key){
+        	fn[key] = temp[name][key];
+      	});
+    	}
+    	temp[name] = fn;
+  	};
+	};
+	this.expect = function(actual) {
 	var to = {};
 	var not = {};
-	this.expect = function(actual) {
+	raw.forEach(build(to,"msg",actual));
+	raw.forEach(build(not,"not",actual));
+	to.not = not;
+	return to;
 	}
 })()
 
