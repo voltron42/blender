e311eacf60d4e70b3cc4aa2d41a5b1f8054cae78
Daniel Johnson <dajohnson1978@gmail.com>
2015-03-13 06:59:53 -0500

    added testing

--- append/Object/object.test.js
+++ append/Object/object.test.js
@@ -1,7 +1,7 @@
 describe("Object prototype extentions",function(){
 describe("apply function",function(){
 it("exclusive",function(){
-var obj = {a:1};
+var obj = {a:1};.
 obj.apply({b:2});
 expect(obj).to.deepEqual({a:1,b:2});
 });
@@ -14,6 +14,22 @@ it("override", function(){
 var obj = {a:1};
 obj.apply({a:2}, true);
 expect(obj).to.deepEqual({a:2});
+});
+});
+describe("copy", function(){
+it("shallow", function(){
+var obj = {a:1};
+var copy = obj.copy();
+expect(copy).to.deepEqual({a:2});
+copy.a = 2;
+expect(obj.a).to.equal(1);
+});
+it("deep",function(){
+var obj = {a:[]};
+var copy = obj.copy();
+expect(copy).to.deepEqual({a:[]});
+copy.a.push(5);
+expect(obj.a).to.have.length(1);
 });
 });
 });
